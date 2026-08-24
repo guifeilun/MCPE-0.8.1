e10a4bd1dcbe893bc2eac6555a369d08cf6536af
guifeilun <guifeilun666@outlook.com>
2026-08-24 20:16:18 +0800

    Update SoundSystemDirectSound.hpp

--- minecraftpe/headers/sound/SoundSystemDirectSound.hpp
+++ minecraftpe/headers/sound/SoundSystemDirectSound.hpp
@@ -1,13 +1,20 @@
 #pragma once
-#ifdef __WIN32__
+#ifdef _WIN32
+  #include <winsock2.h>
+  #include <windows.h>
+  #include <mmsystem.h>
+#endif
+#include <dsound.h>
 #include <_types.h>
 #include <sound/SoundSystem.hpp>
 
 struct SoundSystemDirectSound: public SoundSystem
 {
-	static const int MAX_PLAYED = 8;
+	static const int MAX_PLAYED = 4;
 
-	int playedCnt;
+	LPDIRECTSOUNDBUFFER buffers[MAX_PLAYED] = {0};
+	LPDIRECTSOUND8 dsound = 0;
+	int playedCnt = 0;
 
 	SoundSystemDirectSound(void);
 	virtual ~SoundSystemDirectSound();
@@ -23,4 +30,3 @@ struct SoundSystemDirectSound: public SoundSystem
 	virtual void stop(const std::string&);
 	virtual void playAt(const struct SoundDesc&, float, float, float, float, float);
 };
-#endif
